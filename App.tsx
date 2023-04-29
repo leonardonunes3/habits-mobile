7d6a87da918ae39661ec80166f72467ff2934cb9
Leonardo Nunes <leonardo.nunes793@gmail.com>
2023-04-29 11:48:40 -0300

    loading + fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { useFonts, 
+         Inter_400Regular, 
+         Inter_600SemiBold, 
+         Inter_700Bold, 
+         Inter_800ExtraBold} from '@expo-google-fonts/inter'
+
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Initial configs!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color:'#FFF', fontFamily: 'Inter_800ExtraBold'}}>Initial configs!</Text>
+      <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent/>
     </View>
   );
 }
@@ -13,7 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
