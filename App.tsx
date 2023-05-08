0e51b3c6c210ce95a319a029c6bca7c41f754e0c
Leonardo Nunes <leonardo.nunes793@gmail.com>
2023-05-07 23:33:49 -0300

    Home Screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import './src/lib/dayjs';
+
+import { StatusBar } from 'react-native';
 import { useFonts, 
          Inter_400Regular, 
          Inter_600SemiBold, 
@@ -6,6 +8,7 @@ import { useFonts,
          Inter_800ExtraBold} from '@expo-google-fonts/inter'
 
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,18 +25,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={{color:'#FFF', fontFamily: 'Inter_800ExtraBold'}}>Initial configs!</Text>
+    <>
+      <Home />
       <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent/>
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
